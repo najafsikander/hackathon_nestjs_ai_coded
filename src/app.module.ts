990931f8728f745e250f673bd71cf21d8ee940f2
najafsikander <najafsikander@yahoo.com>
2026-07-30 18:10:13 +0400

    First commit (also configured arcjet)

---
+++ src/app.module.ts
@@ -0,0 +1,43 @@
+import {
+  ArcjetGuard,
+  ArcjetModule,
+  fixedWindow,
+  shield,
+} from '@arcjet/nest';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+
+import 'dotenv/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ArcjetModule.forRoot({
+      isGlobal: true,
+      key: process.env.ARCJET_KEY!,
+      rules: [
+        shield({ mode: 'LIVE' }),
+        fixedWindow({
+          mode: 'LIVE',
+          window: '1m',
+          max: 100,
+        }),
+      ],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ArcjetGuard,
+    },
+  ],
+})
+export class AppModule {}
